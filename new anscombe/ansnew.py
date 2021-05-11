f618df231b1d470cc98fdbf865e3ab9d620c083f
rdobrycdv <56920594+rdobrycdv@users.noreply.github.com>
2021-05-11 14:02:53 +0200

    imports

--- new anscombe/ansnew.py
+++ new anscombe/ansnew.py
@@ -1,28 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from input_data import x, y1, y2, y3, x4, y4, df_dict
 
-x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
-y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
-y2 = [9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
-y3 = [7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73]
-x4 = [8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8]
-y4 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.50, 5.56, 7.91, 6.89]
 
-df_dict = {
-    'I': y1,
-    'II': y2,
-    'III': y3,
-    'IV': y4
-}
-
-def create_dataframe(df_dict: dict):
+def create_dataframe(df_dict):
     anscombe_df = pd.DataFrame(df_dict, index=x)
     return anscombe_df
 
 
 def crete_directory():
     return os.makedirs('results', exist_ok=True)
+
 
 def calculations(anscombe_df):
     results = {'mean': [anscombe_df.mean()],
@@ -37,11 +26,14 @@ def results_to_csv(results):
     return csv_results
 
 
-def results_to_csv(csv_results):
+def saving_results_to_csv(csv_results):
     return csv_results.to_csv(f'results/numeral_results.csv', index=False, header=True, sep=',')
+
 
 def plot(anscombe_df):
     scatter_plot_df = anscombe_df.reset_index()
+    return scatter_plot_df
+
 
 fig, axs = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(6, 6))
 fig.suptitle('Anscombe quartet results', fontsize=16)
